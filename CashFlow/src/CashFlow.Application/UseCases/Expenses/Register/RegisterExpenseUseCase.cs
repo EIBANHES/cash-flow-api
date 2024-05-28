ea929d3743d121be5395d789eca719c97457f1b2
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-05-27 21:08:05 -0400

    Validações

--- CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,16 +1,45 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
 
 public class RegisterExpenseUseCase
 {
-    public ResponseRegisteredExpenseJson Execute(RequestExpenseJson request)
+    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
-        // TO DO VALIDATIONS
-
+        Validate(request);
 
         return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+
+        if (titleIsEmpty)
+        {
+            throw new ArgumentException("The title is required.");
+        }
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount must be greater than zero.");
+        }
+
+        var dateIsValid = DateTime.Compare(request.Date, DateTime.UtcNow);
+
+        if (dateIsValid > 0)
+        {
+            throw new ArgumentException("Expenses cannot be for the future");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+
+        if (!paymentTypeIsValid)
+        {
+            throw new ArgumentException("Payment type is not valid");
+        }
     }
 }
 
---
+++ CashFlow/src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,13 @@
+﻿namespace CashFlow.Communication.Responses;
+
+public class ResponseErrorJson
+{
+    public string ErrorMessage { get; set; } = string.Empty;
+
+    // Construtor, executa uma vez quando instancia uma classe, forçando sempre passar um error message
+    public ResponseErrorJson(string errorMessage)
+    {
+        ErrorMessage = errorMessage;
+    }
+}
+
