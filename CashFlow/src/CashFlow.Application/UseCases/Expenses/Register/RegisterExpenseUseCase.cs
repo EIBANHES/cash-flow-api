f1cd11a6050d9784bc503660ccfd11162225d7ab
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-06-04 18:48:37 -0400

    Utilizando fluent validator

--- CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -15,30 +15,13 @@ public class RegisterExpenseUseCase
 
     private void Validate(RequestRegisterExpenseJson request)
     {
-        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
-
-        if (titleIsEmpty)
+        var validator = new RegisterExpenseValidator();
+        var result = validator.Validate(request);
+        // lista de strings de erros
+        if (result.IsValid == false)
         {
-            throw new ArgumentException("The title is required.");
-        }
-
-        if (request.Amount <= 0)
-        {
-            throw new ArgumentException("The amount must be greater than zero.");
-        }
-
-        var dateIsValid = DateTime.Compare(request.Date, DateTime.UtcNow);
-
-        if (dateIsValid > 0)
-        {
-            throw new ArgumentException("Expenses cannot be for the future");
-        }
-
-        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
-
-        if (!paymentTypeIsValid)
-        {
-            throw new ArgumentException("Payment type is not valid");
+            var errorsMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+            throw new ArgumentException();
         }
     }
 }
