7f53ea6e83d2acd5f9261bb8d83b6407ee79a96b
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-06-05 21:54:20 -0400

    Utilizando teste de unidade, validando o validator

---
+++ CashFlow/tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register
+{
+    public class RegisterExpenseValidatorTests
+    {
+        [Fact] //-> atributo diz que é um teste de unidade
+        public void Success()
+        {
+            //Arrange, configura as instancias que precisa
+            var validator = new RegisterExpenseValidator();
+            var request = new RequestRegisterExpenseJson
+            {
+                Amount = 100,
+                Date = DateTime.Now.AddDays(-1),
+                Description = "description",
+                Title = "title",
+                PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard
+            };
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            Assert.True(result.IsValid);
+        }
+    }
+}
