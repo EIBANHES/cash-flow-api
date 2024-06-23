b31314f1b996676e7280624de470e902f6a79546
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-06-23 11:51:44 -0400

    dbcontext

---
+++ CashFlow/src/CashFlow.Infraestructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infraestructure.DataAccess
+{
+    public class CashFlowDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=Barros2503;";
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 37));
+
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+    }
+}
