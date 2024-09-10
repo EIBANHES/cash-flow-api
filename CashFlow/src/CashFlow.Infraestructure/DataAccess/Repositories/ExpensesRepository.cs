dc62db30517bf57667b849e9c3aa7e3387c7bfb2
Emanuel Barros <emanuel.ibanhes.contato@gmail.com>
2024-09-10 19:27:57 -0400

    GetAll Expenses

--- CashFlow/src/CashFlow.Infraestructure/DataAccess/Repositories/ExpensesRepository.cs
+++ CashFlow/src/CashFlow.Infraestructure/DataAccess/Repositories/ExpensesRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Domain.Entities;
 using CashFlow.Domain.Repositories.Expenses;
+using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Infraestructure.DataAccess.Repositories
 {
@@ -17,5 +18,11 @@ namespace CashFlow.Infraestructure.DataAccess.Repositories
         {
             await _dbContext.Expenses.AddAsync(expense);
         }
+
+        public async Task<List<Expense>> GetAll()
+        {
+           return await _dbContext.Expenses.ToListAsync();
+        }
+
     }
 }
