d617f775e35d60649917b954f2c72a9eb0b2ea52
Emanuel Barros <emanuel.ibanhes.contato@gmail.com>
2024-10-02 15:16:22 -0400

    Utilizando o principio O do SOLID

--- CashFlow/src/CashFlow.Exeception/ExceptionsBase/ErrorOnValidationException.cs
+++ CashFlow/src/CashFlow.Exeception/ExceptionsBase/ErrorOnValidationException.cs
@@ -1,12 +1,21 @@
-﻿namespace CashFlow.Exception.ExceptionsBase
+﻿using System.Net;
+
+namespace CashFlow.Exception.ExceptionsBase
 {
     public class ErrorOnValidationException : CashFlowException
     {
-        public List<string> Errors { get; set; }
+        private CashFlowException _cashFlowExceptionImplementation;
+        private readonly List<string> _erros;
 
+        public override int StatusCode => (int)HttpStatusCode.BadRequest;
         public ErrorOnValidationException(List<string> errorMessages) : base(string.Empty)
         {
-            Errors = errorMessages;
+            _erros = errorMessages;
+        }
+        
+        public override List<string> GetErrors()
+        {
+            return _erros;
         }
     }
 }
