1ef0b980f6e6a7514dfd7304b3259feb2f41e6fa
Emanuel Barros <emanuel.ibanhes.contato@gmail.com>
2024-11-11 14:26:00 -0400

    Feature: Creating reports with excel

---
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Excel/IGenereteExpensesReportExcelUseCase.cs
@@ -0,0 +1,7 @@
+﻿namespace CashFlow.Application.UseCases.Expenses.Reports.Excel
+{
+    public interface IGenereteExpensesReportExcelUseCase
+    {
+        Task<byte[]> Execute(DateOnly month);
+    }
+}
